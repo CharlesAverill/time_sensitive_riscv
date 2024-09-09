d5d69963442b71676769bf1316ad0fe9a41dbec4
Charles Averill <charlesaverill20@gmail.com>
2024-09-08 21:59:47 -0500

    Update build_RTOSDemo.elf.sh

--- FreeRTOS/build_RTOSDemo.elf.sh
+++ FreeRTOS/build_RTOSDemo.elf.sh
@@ -3,6 +3,6 @@ cd FreeRTOS
 git submodule update --checkout --init --recursive
 cd ..
 cp ./Makefile FreeRTOS/FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC/build/gcc
-cp ./FreeRTOSConfig.h FreeRTOS/FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC
+cp ./FreeRTOSConfig.h FreeRTOS/FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC # Only do this if you want to disable configASSERT
 cd FreeRTOS/FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC
 make -C build/gcc
