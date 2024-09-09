ae948f3b52f1192fe5a54e21f3afa6ea06312ef4
Charles Averill <charlesaverill20@gmail.com>
2024-09-08 21:59:28 -0500

    Update build_RTOSDemo.elf.sh

--- FreeRTOS/build_RTOSDemo.elf.sh
+++ FreeRTOS/build_RTOSDemo.elf.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/FreeRTOS/FreeRTOS --depth=1
+cd FreeRTOS
+git submodule update --checkout --init --recursive
+cd ..
 cp ./Makefile FreeRTOS/FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC/build/gcc
 cp ./FreeRTOSConfig.h FreeRTOS/FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC
 cd FreeRTOS/FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC
